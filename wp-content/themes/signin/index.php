5abf839a8b2376e837afe569f537d6daa966b62b
Yasyf Mohamedali <yasyf@yasyf.com>
2012-10-02 15:33:30 -0700

    added more info for curious outsiders

--- wp-content/themes/signin/index.php
+++ wp-content/themes/signin/index.php
@@ -6,11 +6,7 @@ header( 'Expires: 0' );
 /**
  * The main template file.
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ * This file essentially holds no semblance to the original stock WordPress theme, I just used is out of pure laziness, so as to avoid changing any theme settings in my WP install.
  *
  * @package WordPress
  * @subpackage Twenty_Eleven
